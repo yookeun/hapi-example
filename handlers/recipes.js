4643e9ba71af141eb140648dbf4b6e14977d9df9
yookeun <ykkim@andwise.com>
2016-04-29 11:36:24 +0900

    first commit and c2.7

---
+++ handlers/recipes.js
@@ -0,0 +1,52 @@
+'use strict';
+
+exports.find = function(request, reply) {
+  let sql = 'SELECT * FROM recipes';
+  const params = [];
+
+  if (request.query.cuisine) {
+    sql += ' WHERE cuisine = ?';
+    params.push(request.query.cuisine);
+  }
+
+  this.db.all(sql, params, (err, results) => {
+    if (err) {
+        throw err;
+    }
+    reply(results);
+  });
+};
+
+exports.findOne = function(request, reply) {
+  this.db.get('SELECT * FROM recipes WHERE id = ?', [request.params.id], (err, result) => {
+    if (err) {
+      throw err;
+    }
+    if (typeof result !== 'undefined') {
+      return reply(result);
+    }
+    return reply('Not found').code(404);
+  });
+};
+
+exports.create = function(request, reply) {
+  const sql = 'INSERT INTO recipes (name, cooking_time, prep_time, serves, cuisine, ingredients, directions, user_id) VALUES(?, ?, ?, ?, ?, ?, ?, ?)';
+  this.db.run(sql,
+    [
+      request.payload.name,
+      request.payload.cooking_time,
+      request.payload.prep_time,
+      request.payload.serves,
+      request.payload.cuisine,
+      request.payload.ingredients,
+      request.payload.directions,
+      request.payload.user_id
+    ],
+    (err) => {
+      if (err) {
+        throw err;
+      }
+      reply({ status: 'ok'});
+    }
+  );
+};
---
+++ routes.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const Recipes = require('./handlers/recipes');
+
+module.exports = [
+  {
+    method: 'GET',
+    path: '/api/recipes',
+    handler: Recipes.find
+  },
+  {
+    method: 'GET',
+    path: '/api/recipes/{id}',
+    handler: Recipes.findOne
+  },
+  {
+    method: 'POST',
+    path: '/api/recipes',
+    config: {
+      auth: 'api'
+    },
+    handler: Recipes.create
+  }
+]
