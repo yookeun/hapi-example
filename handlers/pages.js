9a4718238ff84e6577dc63d12f90abc901e82237
yookeun <ykkim@andwise.com>
2016-05-13 17:07:35 +0900

    client api commit

--- handlers/pages.js
+++ handlers/pages.js
@@ -1,45 +1,35 @@
 'use strict';
 
-const Wreck = require('wreck');  //Node용 http 클라이언트 모듈 , restapi와 연결을 위해서 사용
+const Wreck = require('wreck');  //HTTP Client Utilities : Node용 http 클라이언트 모듈 , restapi와 연결을 위해서 사용
 
 
+
+/**
+* 초기화면
+*/
 exports.home = function(request, reply) {
   const apiUrl = this.apiBaseUrl + '/recipes';
   Wreck.get(apiUrl, {json: true}, (err, res, payload) => {
     if (err) {
       throw err;
     }
-    console.log("apiUrl====="+apiUrl);
     reply.view('index', {
-      recipies: payload
+      recipes: payload
     });
   });
 };
 
-/*
-exports.home = function(request, reply) {
-  const recipes = [
-    {
-      id: 1,
-      name: 'Sllicate soup',
-      cuisine: 'Martian',
-      serves: 100,
-      prep_time: '2 hours',
-      cooking_time: '12 minutes'
-    },
-    {
-      id: 2,
-      name: 'Methane trifle',
-      cuisine: 'Neptunian',
-      serves: 200,
-      prep_time: '1 hours',
-      cooking_time: '24 minutes'
-    }
-  ];
+exports.viewRecipe = function(request, reply) {
 
-  reply.view('index',{recipes: recipes});
+  const apiUrl = this.apiBaseUrl + '/recipes/'+request.params.id;
+  Wreck.get(apiUrl, {json: true}, (err, res, payload) => {
+    if (err) {
+      throw err;
+    }
+    console.log("request.params.id=="+request.params.id);
+    reply.view('recipe', {recipe: payload});
+  });
 };
-*/
 
 exports.login = function(request, reply) {
   reply.view('login');
--- routes.js
+++ routes.js
@@ -5,35 +5,47 @@ const Pages = require('./handlers/pages');
 const Assets = require('./handlers/assets');
 const Actions = require('./handlers/actions')
 
+
 module.exports = [
   {
     method: 'GET',
     path: '/',
     handler: Pages.home
-  },{
+  },
+  {
     method: 'GET',
     path: '/api/recipes',
     handler: Recipes.find
-  },{
+  },
+  {
     method: 'GET',
     path: '/{param*}',
     handler: Assets.servePublicDirectory
-  },{
+  },
+  {
     method: 'GET',
     path: '/recipes/{id}',
-    handler: Recipes.findOne
-  },{
+    handler: Pages.viewRecipe
+  },
+  {
     method: 'POST',
     path: '/api/recipes',
     config: {
       auth: 'api' //bearer-access-token사용
     },
     handler: Recipes.create
-  },{
+  },
+  {
+    method: 'GET',
+    path: '/api/recipes/{id}',
+    handler: Recipes.findOne
+  },
+  {
     method: 'GET',
     path: '/login',
     handler: Pages.login
-  },{
+  },
+  {
     method: 'POST',
     path: '/login',
     handler: Actions.login
